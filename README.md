6e3493e3a38aab9d950eb162f38b27d3f1f92ace
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-30 14:10:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Codeup PHP Execises
+
+These are the exercises I did while in Codeup.
---
+++ text.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'Welcome to Codeup!' . PHP_EOL;
+echo 'This is my first PHP script!' . PHP_EOL;
+?>
