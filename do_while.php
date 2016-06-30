4f2294a71276ced2336dde3d74cb5b410e0312c3
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-06-30 16:12:04 -0500

    Modified loop to square itself

--- do_while.php
+++ do_while.php
@@ -1,10 +1,8 @@
 <?php
-// Alter your loop to count backwards by 5's from 100 to -10.
-	$a = 100;
+// Create a do-while loop that starts at 2, and displays the result $a * $a on each line while $a is less than 1,000,000.
+	$a = 2;
 	do {
 		echo "$a\n";
-		$a -= 5;
-	}while ($a >= -10);
-
-
+		$a *= $a;
+	}while ($a <= 1000000);
 ?>
