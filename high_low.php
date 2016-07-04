eecf62ca1b5d92755cb4f49fe8b2c1a4c407fd16
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-03 22:03:36 -0500

    Adding high low game. Exercise complete

---
+++ high_low.php
@@ -0,0 +1,21 @@
+<?php
+	$number = mt_rand(1, 50);
+	$guessCount = 1;
+
+	fwrite(STDOUT, "Welcome! Guess my number, it's between 1 and 50. \n");
+	
+		do { 
+			$guess = fgets(STDIN);
+			fwrite(STDOUT, "What is your guess? \n");
+			if ($guess < $number) {
+				echo "Higher!\n";
+				$guessCount++;
+			} elseif ($guess > $number) {
+				echo "Lower!\n";
+				$guessCount++;
+			} else {
+				echo "You got it!\n";
+				echo "Guesses made: " . $guessCount . "\n";
+				echo "To exit game, press Control + C\n";
+			}
+			} while ($guess !== $number);
