f66c5e2977ae1d227319249fc8f762b990cae547
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-05 15:47:27 -0500

    changed variable name from stuff to thing

--- foreach.php
+++ foreach.php
@@ -1,39 +1,39 @@
 <?php
 	$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
-	foreach ($things as $stuff) {
-		if (is_int($stuff)) {
-			echo "{$stuff} is a number\n";
-			} elseif (is_float($stuff)){
-				echo "{$stuff} is a float\n";
-			} elseif (is_bool($stuff)) {
-				echo var_dump($stuff) . "is a boolean\n";
-			} elseif (is_array($stuff)) {
-				echo var_dump($stuff) . "is an array\n";
-			} elseif (is_null($stuff)) {
-				echo var_dump($stuff) . "is null\n";
-			} elseif (is_string($stuff)) {
-				echo "{$stuff} is a string\n";
+	foreach ($things as $thing) {
+		if (is_int($thing)) {
+			echo "{$thing} is a number\n";
+			} elseif (is_float($thing)){
+				echo "{$thing} is a float\n";
+			} elseif (is_bool($thing)) {
+				echo var_dump($thing) . "is a boolean\n";
+			} elseif (is_array($thing)) {
+				echo var_dump($thing) . "is an array\n";
+			} elseif (is_null($thing)) {
+				echo var_dump($thing) . "is null\n";
+			} elseif (is_string($thing)) {
+				echo "{$thing} is a string\n";
 			}
 
 		}
 	echo "\n";
-	
-	foreach ($things as $stuff) {
-		if (is_scalar($stuff)) {
-			echo "{$stuff} is a scalar\n";
+
+	foreach ($things as $thing) {
+		if (is_scalar($thing)) {
+			echo "{$thing} is a scalar\n";
 		}
 	}
 
 	echo "\n";
 
-	foreach ($things as $stuff) {
-		if (is_array($stuff)) {
+	foreach ($things as $thing) {
+		if (is_array($thing)) {
 			echo "Array ";
-			foreach ($stuff as $number) {
+			foreach ($thing as $number) {
 				echo "{$number},";
 			}
 			echo "\n";
 		}else {
-			echo "{$stuff}\n";
+			echo "{$thing}\n";
 		}
 	}
