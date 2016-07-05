115fcb2e66ccbf059ebd31558fcd6430a1231b58
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-05 14:51:47 -0500

    for each loop now determines if input is a scalar

---
+++ foreach.php
@@ -0,0 +1,34 @@
+<?php
+	$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+	foreach ($things as $stuff) {
+		if (is_int($stuff)) {
+			echo "{$stuff} is a number\n";
+			} elseif (is_float($stuff)){
+				echo "{$stuff} is a float\n";
+			} elseif (is_bool($stuff)) {
+				echo var_dump($stuff) . "is a boolean\n";
+			} elseif (is_array($stuff)) {
+				echo var_dump($stuff) . "is an array\n";
+			} elseif (is_null($stuff)) {
+				echo var_dump($stuff) . "is null\n";
+			} elseif (is_string($stuff)) {
+				echo "{$stuff} is a string\n";
+			}
+			
+		}
+	foreach ($things as $stuff) {
+		if (is_scalar($stuff)) {
+			echo "{$stuff} is a scalar\n";
+		} else {
+			echo "{$stuff} is not a scalar\n";
+		}
+	}
+
+
+
+
+
+
+
+
+
