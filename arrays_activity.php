305876fd250385a6267c7e78addf5ed1d9eab8c6
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-05 09:30:13 -0500

    Adding javascript array refactor into php

---
+++ arrays_activity.php
@@ -0,0 +1,10 @@
+<?php
+			$names = ['Chico', 'Lola', 'Max', 'Dexter'];
+				for ($i = 0; i < $names.length; i++) {
+					echo "The dog\'s name is " . $names . PHP_EOL;
+					echo $name[];
+				}
+
+
+
+
