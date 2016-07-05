47cdff634c0d435150a94571980cba6e1a6cb6dc
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-05 10:17:15 -0500

    Refactored code to use user input for guessing number range

--- high_low.php
+++ high_low.php
@@ -1,23 +1,25 @@
 <?php
-	$number = mt_rand(1, 100);
-	$guessCount = 1;
-
-	fwrite(STDOUT, "Welcome! Guess my number, it's between 1 and 100. \n");
+	if ($argc == 3) {
+		$number = mt_rand($argv[1], $argv[2]);
+		$guessCount = 0;
 	
-		do {
-			$guess = trim(fgets(STDIN));
-			fwrite(STDOUT, "What is your guess? \n");
-
-			if ($guess < $number) {
-				fwrite(STDOUT, "Higher!\n");
-			} elseif ($guess > $number) {
-				fwrite(STDOUT, "Lower!\n");
-			} else {
-				fwrite(STDOUT, "You got it!\n");
-				fwrite(STDOUT, "Guesses made: " . $guessCount . "\n");
-				fwrite(STDOUT, "To exit game, press Control + C\n");
-			}
-		 	$guessCount++;
-		} while ($guess !== $number);
+		fwrite(STDOUT, "Guess my number\n");
+		
+			do {
+				$guess = trim(fgets(STDIN));
+				fwrite(STDOUT, "What is your guess? \n");
+	
+				if ($guess < $number) {
+					fwrite(STDOUT, "Higher!\n");
+				} elseif ($guess > $number) {
+					fwrite(STDOUT, "Lower!\n");
+				} else {
+					fwrite(STDOUT, "You got it!\n");
+					fwrite(STDOUT, "Guesses made: " . $guessCount . "\n");
+					fwrite(STDOUT, "To exit game, press Control + C\n");
+				}
+			 	$guessCount++;
+			} while ($guess !== $number);
+	}
 ?>
 
