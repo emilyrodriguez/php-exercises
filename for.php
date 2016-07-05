64d3b2fd5e965f53d4ab307b3e0617085ece4501
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-05 13:56:59 -0500

    Refactored to count by user input, will only work if numeric numbers are entered

--- for.php
+++ for.php
@@ -1,11 +1,19 @@
 <?php
-	$min = 1;
-	$max = 3;
 	fwrite(STDOUT, "Enter a starting number\n");
 	$min = trim(fgets(STDIN));
 	fwrite(STDOUT, "Enter an ending number\n");
 	$max = trim(fgets(STDIN));
+	fwrite(STDOUT, "Enter a number to increment by\n");
+	$counter = trim(fgets(STDIN));
 
-	for ($i = $min; $i <= $max; $i++) {
-		fwrite(STDOUT, $i . PHP_EOL);
+	if (!is_numeric($counter)) {
+		$counter = 1;
+	}
+	
+	if (is_numeric($min) && is_numeric($max) && $min < $max) {
+		for ($i = $min; $i <= $max; $i += $counter) {
+			fwrite(STDOUT, $i . PHP_EOL);
+		} 
+	}else {
+		fwrite(STDERR, "You must enter numeric values.");
 	}
