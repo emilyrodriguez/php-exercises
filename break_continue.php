0d4e8172f45bd2ba77794178d084021d8d789f61
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-06 10:08:41 -0500

    Loop counts to 100 but only prints to 10

--- break_continue.php
+++ break_continue.php
@@ -1,9 +1,8 @@
 <?php
-
-for ($i = 1; $i <= 100; $i++) {
-	if ($i % 2 == 1) {
-		continue;
+	for ($i = 1; $i <= 100; $i++){
+		echo $i . "\n";
+		if ($i == 10) {
+			break;
+		}
 	}
-		echo $i . " is an even number. \n";
-}
-
+?>
