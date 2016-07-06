84eb6587d72b92876ac4f92d5c587b39dbf00ddb
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-06 10:40:27 -0500

    code refactored into if/elseif statemente

--- switch.php
+++ switch.php
@@ -6,26 +6,18 @@
  // 1 (for Monday) through 7 (for Sunday)
  $dayOfWeek = date('N');
 
- switch($dayOfWeek) {
-     case 1:
-        echo "Monday\n";
-        break;
-     case 2:
-     	echo "Tuesday\n";
-     	break;
-     case 3:
-     	echo "Wednesday\n";
-     	break;
-     case 4:
-     	echo "Thursday\n";
-     	break;
-     case 5:
-     	echo "Friday\n";
-     	break;
-     case 6:
-     	echo "Saturday\n";
-     	break;
-     case 7:
-     	echo "Sunday\n";
-     	break;
+ if ($dayOfWeek == 1) {
+ 	echo "It's Monday!\n";
+ } elseif ($dayOfWeek == 2) {
+ 	echo "It's Tuesday!\n";
+ } elseif ($dayOfWeek == 3) {
+ 	echo "It's Wednesday!\n";
+ } elseif ($dayOfWeek == 4) {
+ 	echo "It's Thursday!\n";
+ } elseif ($dayOfWeek == 5) {
+ 	echo "It's Friday!\n";
+ }  elseif ($dayOfWeek == 6) {
+ 	echo "It's Saturday!\n";
+ } elseif ($dayOfWeek == 7) {
+ 	echo "It's Sunday!\n";
  }
