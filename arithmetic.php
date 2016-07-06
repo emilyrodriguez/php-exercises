091cba6cd65248458c402bc0ed2b463ec9939741
Emily Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-06 11:18:26 -0500

    created functions that do math for variables with given values

---
+++ arithmetic.php
@@ -0,0 +1,30 @@
+<?php
+$a = 5;
+$b = 10;
+
+
+ function add($a, $b)
+ {
+     return $a + $b;
+ }
+
+ function subtract($a, $b)
+ {
+      return $a - $b;
+ }
+
+ function multiply($a, $b)
+ {
+      return $a * $b;
+ }
+
+ function divide($a, $b)
+ {
+      return $a / $b;
+ }
+ function modulus($a, $b)
+ {
+      return $a % $b;
+ }
+
+echo add($a, $b), PHP_EOL;
