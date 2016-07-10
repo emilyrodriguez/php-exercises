d6139fa46cfa8d2d1731acb7d64ebdecc5b2af34
Emily A Rodriguez <emilyrodriguez@MacBook-Pro.local>
2016-07-10 15:07:18 -0500

    Code now gives an error if numbers aren't numeric or user tries to divide by 0

--- arithmetic.php
+++ arithmetic.php
@@ -2,29 +2,61 @@
 $a = 5;
 $b = 10;
 
+function errorMessage ($a, $b) {
+	if ($b === 0) {
+		fwrite(STDERR, "ERROR: Cannot divide by " . $b . "!\n");
+	} elseif (!is_numeric($a)) {
+		fwrite(STDERR, "Error: " . $a . " is not a number!\n");
+	} elseif (!is_numeric($b)) {
+		fwrite(STDERR, "Error: " . $b . " is not a number!\n");
+	}
+}
 
- function add($a, $b)
- {
-     return $a + $b;
- }
+function add($a, $b){
+ 	if (is_numeric($a) && is_numeric($b)) {
+ 		return $a + $b;
+ 	} else {
+ 		errorMessage($a, $b);
+ 	}
+ 	
+}
 
- function subtract($a, $b)
- {
-      return $a - $b;
- }
+function subtract($a, $b){
+ 	if (is_numeric($a) && is_numeric($b)) {
+ 		return $a - $b;
+ 	} else {
+ 		errorMessage($a, $b);
+ 	}
+}
 
- function multiply($a, $b)
- {
-      return $a * $b;
- }
+function multiply($a, $b){
+ 	if (is_numeric($a) && is_numeric($b)) {
+ 		return $a * $b;
+ 	} else {
+ 		errorMessage($a, $b);
+ 	}
+}
+ 	
+function divide($a, $b){
+ 	if (is_numeric($a) && is_numeric($b)) {
+ 		return $a / $b;
+ 	} else {
+ 		errorMessage($a, $b);
+ 	}
+}
 
- function divide($a, $b)
- {
-      return $a / $b;
- }
- function modulus($a, $b)
- {
-      return $a % $b;
- }
+ function modulus($a, $b){
+ 	if (is_numeric($a) && is_numeric($b)) {
+ 		return $a % $b;
+ 	} else {
+ 		errorMessage($a, $b);
+ 	}
+}
+ 	
 
-echo add($a, $b), PHP_EOL;
+echo "Add: " . add($a, $b), PHP_EOL;
+echo "Subtract: " . subtract($a, $b), PHP_EOL;
+echo "Multiply: " . multiply($a, $b), PHP_EOL;
+echo "Divide: " . divide($a, $b), PHP_EOL;
+echo "Modulus: " . modulus($a, $b), PHP_EOL;
+
