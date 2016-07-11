6ca086fae07cd4036a5572d392fe7156bb8924be
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-11 11:01:24 -0500

    turned code into a function

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,9 +1,18 @@
 <?php
 
-$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+function humanizedList($physicistsArray) {
+	$famousFakePhysicists = array_pop($physicistsArray);
+	$famousFakePhysicists = array_push($physicistsArray, " and Tony Stark");
+	$famousFakePhysicists = implode(',', $physicistsArray);
+	return $famousFakePhysicists;
+}
 
+$physicistsString = ' Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+$physicistsArray = [];
 $physicistsArray = explode(',', $physicistsString);
-$famousFakePhysicists = array_pop($physicistsArray);
-$famousFakePhysicists = array_push($physicistsArray, " and Tony Stark");
-$famousFakePhysicists = implode(',', $physicistsArray);
+$famousFakePhysicists = humanizedList($physicistsArray);
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
+
+
