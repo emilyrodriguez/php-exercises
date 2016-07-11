5e1279084234b9a5b28fec95f1bd0bb5d8ff13c3
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-11 12:07:23 -0500

    code now optionally sorts names

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,22 +1,23 @@
 <?php
 
-function humanizedList($physicistsArray) {
+function humanizedList($physicistsArray, $sort = false) {
+	if ($sort) {
+		sort($physicistsArray);
+	}
 	$famousFakePhysicists = array_pop($physicistsArray);
-	sort($physicistsArray);
-	print_r($physicistsArray);
 	$famousFakePhysicists = array_push($physicistsArray, " and Tony Stark");
+	
 	$famousFakePhysicists = implode(',', $physicistsArray);
 	return $famousFakePhysicists;
 }
+
 
 $physicistsString = ' Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 $physicistsArray = [];
 $physicistsArray = explode(',', $physicistsString);
-$famousFakePhysicists = humanizedList($physicistsArray);
+$famousFakePhysicists = humanizedList($physicistsArray, true);
 
 
-echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
-
-
-
+echo "Some of the most famous fictional theoretical physicists are{$famousFakePhysicists}.\n";
+?>
